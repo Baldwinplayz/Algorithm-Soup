4cd8de8155290f613990c0e9bbfece2608847f26
Baldwin Huang <125613173+Baldwinplayz@users.noreply.github.com>
2024-12-18 12:02:36 -0400

    Added /ip route

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import requests
+import json
 from flask import Flask
 from render_template import render_template
 
@@ -9,6 +11,10 @@ import searching
 @app.route("/")
 def index():
     return render_template(0, "pages/index.html", "Algorithm Soup")
+
+@app.route("/ip")
+def ip():
+    return json.loads(requests.get("https://api.ipify.org?format=json").text)["ip"]
 
 if "__main__" == __name__:
     app.run(host="0.0.0.0", port=80, debug=True)
